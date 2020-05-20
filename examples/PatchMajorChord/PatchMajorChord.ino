efa4e65ab5db847a37a93a7e18b93f41b1860f0a
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-05-20 12:43:43 -0700

    added two more examples.

---
+++ examples/PatchMajorChord/PatchMajorChord.ino
@@ -0,0 +1,55 @@
+#include "AUDIO.h"
+
+#define NUM_OUTS 4
+
+float mtof(float m) { return pow(2, (m -69.0) / 12.0) * 440.0; }
+
+class Sine
+{
+  public:
+  void Init() { phs = 0.0; SetFreq(100);}
+  void SetFreq(float freq) { phs_inc = (2.0 * PI * freq) * (1.0 / 48000.0); }
+  float Process() { float out; out = sin(phs) * 0.5; phs = phs + phs_inc; if (phs > (2.0 * PI)) { phs -= 2.0 * PI; }; return out; }
+  private:
+  float phs, phs_inc;
+};
+
+size_t num_channels;
+unsigned long knobval;
+Sine osc[NUM_OUTS];
+float maj_chord[NUM_OUTS] = {0, 4, 7, 11};
+
+void SineCallback(float **in, float **out, size_t size)
+{
+    float root = 60;
+    root = 36.0 + (((1023 - knobval) / 1023.0) * 60.0);
+    for (size_t chn = 0; chn < NUM_OUTS; chn++)
+    {
+      osc[chn].SetFreq(mtof(root + maj_chord[chn]));
+    }
+    for (size_t i = 0; i < size; i++)
+    {
+      // Synthesize sine wave per channel
+      for (size_t chn = 0; chn < NUM_OUTS; chn++)
+      {
+        out[chn][i] = osc[chn].Process();
+      }
+    }
+}
+
+void setup()
+{
+    num_channels = AUDIO.init(DAISY_PATCH, AUDIO_SR_48K); 
+    osc[0].Init();
+    osc[1].Init();
+    osc[2].Init();
+    osc[3].Init();
+    AUDIO.begin(SineCallback);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+  knobval = analogRead(A0);
+  Serial.println(knobval);
+}
---
+++ examples/PatchSine/PatchSine.ino
@@ -0,0 +1,35 @@
+#include "AUDIO.h"
+
+size_t num_channels;
+float phs_inc, phs, sinout;
+
+void SineCallback(float **in, float **out, size_t size)
+{
+    phs_inc = (2.0 * PI * 100.0) * (1.0 / 48000.0);
+    for (size_t i = 0; i < size; i++)
+    {
+        // Synthesize sine wave.
+        sinout = sin(phs) * 0.5;
+        // update phase
+        phs = phs + phs_inc;
+        if (phs > 2.0 * PI)
+            phs -= (2.0 * PI)
+
+        // Set to outputs.
+        out[0][i] = sinout;
+        out[1][i] = sinout;
+        out[2][i] = sinout;
+        out[3][i] = sinout;
+    }
+}
+
+void setup()
+{
+    num_channels = AUDIO.init(DAISY_PATCH, AUDIO_SR_48K); 
+    AUDIO.begin(SineCallback);
+}
+
+void loop()
+{
+}
+
