84c87cdcd89d433cdcb2c095d30a64f9c0eb6e08
beserge <bensergentanis@gmail.com>
2020-07-10 14:28:45 -0400

    encoder and switches, encoder example, improved pod class

--- examples/Pod/Encoder/Encoder.ino
+++ examples/Pod/Encoder/Encoder.ino
@@ -1,51 +1,54 @@
 #include "DaisyAudio.h"
 
-AudioClass::Pod pod;
+Pod pod;
 
 uint8_t color;
+
+Encoder enc; //turn the encoder
+Switch sw; //press the encoder
 
 void setup()
 {
     DAISY.init(DAISY_POD, AUDIO_SR_48K); 
-    pod = DAISY.pod;
+    pod.Init(1000);
 
     color = 0;
 
     //setup I/O
     for (int i = 0; i < 3; i++)
     {
-        pinMode(pod.leds[0][i], OUTPUT);
-        pinMode(pod.leds[1][i], OUTPUT);
+        pinMode(pod.ledPins[0][i], OUTPUT);
+        pinMode(pod.ledPins[1][i], OUTPUT);
     }
 
-    pinMode(pod.encoder_inc[0], INPUT_PULLUP);
-    pinMode(pod.encoder_inc[1], INPUT_PULLUP);
-    pinMode(pod.encoder_click, INPUT_PULLUP);
+    pinMode(pod.encIncPins[0], INPUT_PULLUP);
+    pinMode(pod.encIncPins[1], INPUT_PULLUP);
+    pinMode(pod.encClickPin, INPUT_PULLUP);
 }
 
 void SetColor()
 {
-    digitalWrite(pod.leds[0][0], (color & B100) != B100);
-    digitalWrite(pod.leds[0][1], (color & B010) != B010);
-    digitalWrite(pod.leds[0][2], (color & B001) != B001);
+    digitalWrite(pod.ledPins[0][0], (color & B100) != B100);
+    digitalWrite(pod.ledPins[0][1], (color & B010) != B010);
+    digitalWrite(pod.ledPins[0][2], (color & B001) != B001);
 
-    digitalWrite(pod.leds[1][0], (color & B100) != B100);
-    digitalWrite(pod.leds[1][1], (color & B010) != B010);
-    digitalWrite(pod.leds[1][2], (color & B001) != B001);
+    digitalWrite(pod.ledPins[1][0], (color & B100) != B100);
+    digitalWrite(pod.ledPins[1][1], (color & B010) != B010);
+    digitalWrite(pod.ledPins[1][2], (color & B001) != B001);
 }
 
 void loop()
 {
-    if (! digitalRead(pod.encoder_click))
+    pod.encSwitch.Process(digitalRead(pod.encClickPin));
+    pod.enc.Process(digitalRead(pod.encIncPins[0]), digitalRead(pod.encIncPins[1]));
+
+    if (pod.encSwitch.RisingEdge())
     {
         color = 0;
     }
 
-    else if (! digitalRead(pod.encoder_inc[0]))
-    {
-        color++;
-        color %= 8;
-    }
+    color += pod.enc.Increment();
+    color = (color % 8 + 8) % 8;
 
     SetColor();
 }
--- src/DaisyAudio.h
+++ src/DaisyAudio.h
@@ -26,6 +26,122 @@ enum DaisyAudioSampleRate {
 typedef void (*DaisyAudioCallback)(float**, float**, size_t);
 
 
+//TODO
+// encoder increment
+// updateanalogcontrols?
+// PWM for leds would be nice
+
+
+//just does the increment stuff
+//the user will use the switch for the pressed themselves
+class Encoder
+{
+    public:
+
+    void Init()
+    {
+	inc_ = 0;
+	a_ = b_ = 0xff;
+    }
+    
+    void Process(uint8_t a_in, uint8_t b_in)
+    {
+	a_ = (a_ << 1) | (a_in);
+	b_ = (b_ << 1) | (b_in);
+
+	inc_ = 0;
+	
+	if((b_ == 0x7f) && (a_  == 0x00))
+	    inc_ = 1;
+	else if ((a_ == 0x7f) && (b_ == 0x00))
+	    inc_ = -1;
+    }
+
+    int32_t Increment() { return inc_; }
+    
+    private:    
+        uint8_t a_, b_;
+	int32_t inc_;
+};
+
+class Switch
+{
+    public:
+        void Init(float update_rate, bool invert)
+	{
+	    flip_ = invert;
+	    time_per_update_ = 1.f / update_rate;
+	    state_ = 0;
+	    time_held_ = 0.f;
+	}
+
+	//debounces and processes input
+	void Process(uint8_t in)
+	{
+	    state_ = (state_ << 1) | (flip_ ? !in : in);
+	    
+	    if (state_ == 0x7f || state_ == 0x80)
+		time_held_ = 0;
+	    
+	    if (state_ == 0xff)
+		time_held_ += time_per_update_;
+	}
+    
+	bool RisingEdge() { return state_ == 0x7f; }
+	
+	bool FallingEdge() { return state_ == 0x80; }
+	
+	bool Pressed() { return state_ == 0xff; }
+	
+	float TimeHeldMs() { return Pressed() ? time_held_ * 1000.f : 0; }
+	
+    private:
+	bool flip_;
+	float time_per_update_, time_held_;
+	uint8_t state_;
+};
+
+class Pod
+{
+public:
+    int knobPins[2];
+    int switchPins[2];
+    int ledPins[2][3]; //2 leds, RGB pins
+    int encClickPin;
+    int encIncPins[2]; //A, B
+
+    Switch switches[2];
+    
+    Switch encSwitch;
+    Encoder enc;
+    
+    void Init(float control_update_rate)
+    {
+	knobPins[0] = 21;
+	knobPins[1] = 15;
+	
+	switchPins[0] = 27;
+	switchPins[1] = 28;
+	
+	ledPins[0][0] = 20;
+	ledPins[0][1] = 19;
+	ledPins[0][2] = 18;
+	ledPins[1][0] = 17;
+	ledPins[1][1] = 24;
+	ledPins[1][2] = 23;
+	
+	encClickPin = 13;
+	encIncPins[0] = 26; //A
+	encIncPins[1] = 25; //B
+	    
+        switches[0].Init(control_update_rate, true);
+	switches[1].Init(control_update_rate, true);
+
+	encSwitch.Init(control_update_rate, true);
+	enc.Init();
+    }
+};
+
 class AudioClass
 {
     public:
@@ -36,16 +152,6 @@ class AudioClass
         void begin(DaisyAudioCallback cb);
         void end();
         float get_samplerate();
-
-	struct Pod
-	{
-	    int knobs[2];
-	    int switches[2];
-	    int leds[2][3]; //2 leds, RGB pins
-	    int encoder_click;
-	    int encoder_inc[2]; //A, B
-	};
-	Pod pod;
 	
     private:
         size_t _blocksize;
