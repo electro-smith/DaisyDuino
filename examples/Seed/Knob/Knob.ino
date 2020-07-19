e6ee0a1170fc5f13aca3500e23eac28a00af4e10
beserge <bensergentanis@gmail.com>
2020-07-18 22:32:14 -0400

    cleanup

--- examples/Seed/Knob/Knob.ino
+++ examples/Seed/Knob/Knob.ino
@@ -1,20 +1,10 @@
 #include "DaisyDuino.h"
 
-size_t num_channels;
-
-DaisyHardware hw;
-
 uint8_t led_pin;
 uint8_t knob_pin;
 
 void setup()
 {    
-    float sample_rate;
-    // Initialize for Daisy seed at 48kHz
-    hw = DAISY.init(DAISY_SEED, AUDIO_SR_48K);
-    num_channels = hw.num_channels;
-    sample_rate = DAISY.get_samplerate();
-
     led_pin = 28;
     knob_pin = 21;
 
@@ -26,10 +16,12 @@ void loop(){
     // The led brightness will be a value 0 - 1023
     unsigned int brightness = analogRead(knob_pin);
 
+    //simple PWM led brightness
     for (int i = 0; i < brightness; i++)
     {
         digitalWrite(led_pin, HIGH);
     }
+    
     for (int i = 0; i < 1023 - brightness; i++)
     {
         digitalWrite(led_pin, LOW);
