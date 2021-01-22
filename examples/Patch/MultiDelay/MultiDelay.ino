194e60a67a2e0d8bdcea130af037e64251424e49
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-22 16:06:45 -0500

    multidelay

--- examples/Patch/MultiDelay/MultiDelay.ino
+++ examples/Patch/MultiDelay/MultiDelay.ino
@@ -1,14 +1,24 @@
+//Ctrl 1-3: Delay time 1-3
+//Ctrl 4: Feedback amount all
+//Encoder: Dry/wet
+//In 1: In all
+//Out 1-3: Delay 1-3 out
+//Out 4: Mix out.
 
 #include "DaisyDuino.h"
 #include <string>
+#include <U8g2lib.h>
+U8G2_SSD1309_128X64_NONAME2_F_4W_SW_SPI oled(U8G2_R0, 8, 10, 7, 9, 30);
 
 #define MAX_DELAY static_cast<size_t>(48000 * 1.f)
 
 DaisyHardware patch;
 
+float samplerate;
+
 DelayLine<float, MAX_DELAY> DSY_SDRAM_BSS delMems[3];
 
-struct delay {
+struct del {
   DelayLine<float, MAX_DELAY> *del;
   float currentDelay;
   float delayTarget;
@@ -25,8 +35,7 @@ struct delay {
   }
 };
 
-delay delays[3];
-Parameter params[4];
+del delays[3];
 
 float feedback;
 int drywet;
@@ -54,66 +63,58 @@ static void AudioCallback(float **in, float **out, size_t size) {
   }
 }
 
-void InitDelays(float samplerate) {
+void InitDelays() {
   for (int i = 0; i < 3; i++) {
     // Init delays
     delMems[i].Init();
     delays[i].del = &delMems[i];
-    // 3 delay times
-    params[i].Init(patch.controls[i], samplerate * .05, MAX_DELAY,
-                   Parameter::LOGARITHMIC);
   }
-
-  // feedback
-  params[3].Init(patch.controls[3], 0, 1, Parameter::LINEAR);
 }
 
-void UpdateOled();
-
-int main(void) {
-  float samplerate;
-  patch = DAISY.init(
-      DAISY_PATCH, AUDIO_SR_48K); // Initialize hardware (daisy seed, and patch)
+void setup(){
+  patch = DAISY.init(DAISY_PATCH, AUDIO_SR_48K);
   samplerate = DAISY.get_samplerate();
 
-  InitDelays(samplerate);
+  InitDelays();
 
   drywet = 50;
-
+  
+  oled.setFont(u8g2_font_t0_12_mf);
+  oled.setFontDirection(0);
+  oled.setFontMode(1);
+  oled.begin();
+  
   DAISY.begin(AudioCallback);
-  while (1) {
-    UpdateOled();
-  }
 }
 
-void UpdateOled() {
-  patch.display.Fill(false);
+void loop(){
+  oled.clearBuffer();
 
-  patch.display.SetCursor(0, 0);
   std::string str = "Multi Delay";
   char *cstr = &str[0];
-  patch.display.WriteString(cstr, Font_7x10, true);
+  oled.drawStr(0,8,cstr);
 
-  patch.display.SetCursor(0, 30);
   str = "Dry/Wet:  ";
-  patch.display.WriteString(cstr, Font_7x10, true);
+  oled.drawStr(0,38,cstr);
 
-  patch.display.SetCursor(60, 30);
   str = std::to_string(drywet);
-  patch.display.WriteString(cstr, Font_7x10, true);
+  oled.drawStr(60, 38, cstr);
 
-  patch.display.Update();
+  oled.sendBuffer();
 }
 
 void ProcessControls() {
-  patch.ProcessAnalogControls();
-  patch.ProcessDigitalControls();
+  patch.DebounceControls();
 
+  int ctrls[3] = {PIN_PATCH_CTRL_1, PIN_PATCH_CTRL_2, PIN_PATCH_CTRL_3};
+  
   // knobs
   for (int i = 0; i < 3; i++) {
-    delays[i].delayTarget = params[i].Process();
+    float val = (1023.f - analogRead(ctrls[i])) / 1023.f;
+    float m = (float)MAX_DELAY - .05f * samplerate;
+    delays[i].delayTarget = val * m + .05 * samplerate; //.05 * sr to MAX_DELAY
   }
-  feedback = params[3].Process();
+  feedback = (1023.f - analogRead(PIN_PATCH_CTRL_4)) / 1023.f;
 
   // encoder
   drywet += 5 * patch.encoder.Increment();
