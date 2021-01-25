acc06e9d1481624621a7358a3d59f26035171fb4
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-25 11:07:30 -0500

    quadmixer

--- examples/Patch/QuadMixer/QuadMixer.ino
+++ examples/Patch/QuadMixer/QuadMixer.ino
@@ -1,13 +1,20 @@
+// Input  1-4 = channel 1-4 in
+// Ctrl   1-4 = channel 1-4 level
+// Output 1-4 = Mixer output
 
 #include "DaisyDuino.h"
+#include <U8g2lib.h>
+
+U8G2_SSD1309_128X64_NONAME2_F_4W_SW_SPI oled(U8G2_R0, 8, 10, 7, 9, 30);
 
 DaisyHardware patch;
-
+float ctrlVal[4];
 static void AudioCallback(float **in, float **out, size_t size) {
-  float ctrlVal[4];
+  int pins[4] = {PIN_PATCH_CTRL_1, PIN_PATCH_CTRL_2, PIN_PATCH_CTRL_3, PIN_PATCH_CTRL_4};
+
   for (int i = 0; i < 4; i++) {
     // Get the four control values
-    ctrlVal[i] = patch.controls[i].Process();
+    ctrlVal[i] = (float)(1023 - analogRead(pins[i])) / 1023.f;
   }
 
   for (size_t i = 0; i < size; i++) {
@@ -27,16 +34,27 @@ static void AudioCallback(float **in, float **out, size_t size) {
   }
 }
 
-int main(void) {
-  patch = DAISY.init(
-      DAISY_PATCH, AUDIO_SR_48K); // Initialize hardware (daisy seed, and patch)
+void setup(){
+  patch = DAISY.init(DAISY_PATCH, AUDIO_SR_48K);
+
+  oled.setFont(u8g2_font_t0_12_mf);
+  oled.setFontDirection(0);
+  oled.setFontMode(1);
+  oled.begin();
 
   // start audio
-
   DAISY.begin(AudioCallback);
+}
 
-  while (1) {
-    // display the four control levels
-    patch.DisplayControls(false);
+void loop(){
+  // display the four control levels
+  oled.clearBuffer();
+
+  char cstr[15];
+  for(int i = 0; i < 4; i++){
+    sprintf(cstr, "channel %d: %d", i + 1, (int)(ctrlVal[i] * 101.f));
+    oled.drawStr(0, (i + 1) * 10, cstr);
   }
+  
+  oled.sendBuffer();
 }
