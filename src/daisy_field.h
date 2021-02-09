f6f3b04ecdd80e0adf6c14e8bd97f50cd2b52056
U-MSI\Ben <bensergentanis@gmail.com>
2021-02-09 15:27:12 -0500

    add (broken) field support via ctrls
    Adds Mux inits for ctrl, these will use the control muxer
    Adds daisy_field.h with the field's pin defines
    Adds adc.h (for now) to try and get the muxer working.
    I hope I won't need it, since I'm pretty sure gpio interferes with
    the arduino I/O

---
+++ src/daisy_field.h
@@ -0,0 +1,33 @@
+#define PIN_FIELD_GATE_IN 0
+
+#define PIN_FIELD_SPI_CS 7
+#define PIN_FIELD_SPI_SCK 8
+#define PIN_FIELD_OLED_CMD 9
+#define PIN_FIELD_SPI_MOSI 10
+#define PIN_FIELD_I2C_SCL 11
+#define PIN_FIELD_I2C_SDA 12
+
+#define PIN_FIELD_MIDI_OUT 13
+#define PIN_FIELD_MIDI_IN 14
+
+#define PIN_FIELD_GATE_OUT 15
+
+#define PIN_FIELD_ADC_POT_MUX 16
+#define PIN_FIELD_ADC_CV_1 17
+#define PIN_FIELD_ADC_CV_2 18
+
+#define PIN_FIELD_MUX_SEL_2 19
+#define PIN_FIELD_MUX_SEL_1 20
+#define PIN_FIELD_MUX_SEL_0 21
+
+#define PIN_FIELD_CD4021_D1 26
+#define PIN_FIELD_CD4021_CS 27
+#define PIN_FIELD_CD4021_CLK 28
+
+#define PIN_FIELD_SW_2 29
+#define PIN_FIELD_SW_1 30
+
+#define PIN_FIELD_DAC_2 22    // Jumped on Rev2 from 24
+#define PIN_FIELD_DAC_1 23    // Jumped on Rev2 from 25
+#define PIN_FIELD_ADC_CV_4 24 // Jumped on Rev2 from 22
+#define PIN_FIELD_ADC_CV_3 25 // Jumped on Rev2 from 23
--- src/utility/ctrl.cpp
+++ src/utility/ctrl.cpp
@@ -13,6 +13,8 @@ void AnalogControl::Init(uint8_t pin,
 	pinMode(pin, INPUT);
 	pin_ = pin;
 	
+	mux_ = false;
+	
     val_        = 0.0f;
     samplerate_ = sr;
     coeff_      = 1.0f / (slew_seconds * samplerate_ * 0.5f);
@@ -21,6 +23,25 @@ void AnalogControl::Init(uint8_t pin,
     flip_       = flip;
     invert_     = invert;
 }
+
+void AnalogControl::InitMux(uint16_t* adcptr,
+                         float     sr,
+                         bool      flip,
+                         bool      invert,
+                         float     slew_seconds)
+{
+	mux_ = true;
+	
+	val_        = 0.0f;
+    raw_        = adcptr;
+    samplerate_ = sr;
+    coeff_      = 1.0f / (slew_seconds * samplerate_ * 0.5f);
+    scale_      = 1.0f;
+    offset_     = 0.0f;
+    flip_       = flip;
+    invert_     = invert;
+}
+
 
 void AnalogControl::InitBipolarCv(uint8_t pin, float sr)
 {
@@ -39,7 +60,14 @@ void AnalogControl::InitBipolarCv(uint8_t pin, float sr)
 float AnalogControl::Process()
 {
     float t;
-    t = (float)analogRead(pin_) / 1023.f;
+
+	if(mux_){
+		t = (float)*raw_ / 65536.f;
+	}
+	else{
+		t = (float)analogRead(pin_) / 1023.f;
+	}
+
     if(flip_)
         t = 1.f - t;
     t = (t - offset_) * scale_ * (invert_ ? -1.0f : 1.0f);
