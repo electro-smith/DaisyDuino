1795526367945fe1f729ad2bb47e2774453ddccd
beserge <bensergentanis@gmail.com>
2021-10-20 11:01:30 -0400

    Update Pot Input to use voltage conversion function

--- examples/Patch SM/Pot_Input/Pot_Input.ino
+++ examples/Patch SM/Pot_Input/Pot_Input.ino
@@ -17,18 +17,18 @@ void loop()
     /** Update all inputs */
     patch.ProcessAllControls();
 
-    /** Get CV_1 Input (512, 0) */
+    /** Get CV_1 Input */
     int value = analogRead(PIN_PATCH_SM_CV_1);
 
-    /** Flip the range (512, 0) -> (0, 512) */
-    value = 512 - value;
+    /** Convert the AnalogRead range to volts */
+    float volts = patch.AnalogReadToVolts(value);
 
     /** Here the pot is wired to GND and 5V
         * So 2.5V is the pot's halfway point.
         * 
-        * If the Adc value is greater than 256 (512/2)... 
+        * If the Adc value is greater than 2.5 V... 
     */
-    if(value > 256)
+    if(volts > 2.5)
     {
         /** Turn the led on */
         digitalWrite(LED_BUILTIN, HIGH);
