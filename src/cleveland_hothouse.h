df5760d4f5b7c58220666910dd1d68d7480f4ff7
Chip Audette <chipaudette@yahoo.com>
2024-09-23 18:21:03 -0400

    Cleveland Hothouse: re-order the switches so that it is more like Daisy's "Petal"

--- src/cleveland_hothouse.h
+++ src/cleveland_hothouse.h
@@ -1,22 +1,29 @@
 
+//Pins for the "Hothouse" pedal from the Cleveland Music Co.
+//https://clevelandmusicco.com/hothouse-diy-digital-signal-processing-platform-kit/
+//This has a Daisy Seed at its heart and adds:
+//  * 6 potentiometers
+//  * 3 toggle switches up-middle-down (on-off-on)
+//  * 2 momentary footswitches
+//  * 2 LEDs, one over each footswtich
 
+#define PIN_HOTHOUSE_FSW_1 25
+#define PIN_HOTHOUSE_FSW_2 26
 #define PIN_HOTHOUSE_SW_1_UP 9
 #define PIN_HOTHOUSE_SW_1_DOWN 10
 #define PIN_HOTHOUSE_SW_2_UP 7
 #define PIN_HOTHOUSE_SW_2_DOWN 8
 #define PIN_HOTHOUSE_SW_3_UP 5
 #define PIN_HOTHOUSE_SW_3_DOWN 6
-#define PIN_HOTHOUSE_FSW_1 25
-#define PIN_HOTHOUSE_FSW_2 26
 
-#define PIN_HOTHOUSE_SWITCH_1 PIN_HOTHOUSE_SW_1_UP
-#define PIN_HOTHOUSE_SWITCH_2 PIN_HOTHOUSE_SW_1_DOWN
-#define PIN_HOTHOUSE_SWITCH_3 PIN_HOTHOUSE_SW_2_UP
-#define PIN_HOTHOUSE_SWITCH_4 PIN_HOTHOUSE_SW_2_DOWN
-#define PIN_HOTHOUSE_SWITCH_5 PIN_HOTHOUSE_SW_3_UP
-#define PIN_HOTHOUSE_SWITCH_6 PIN_HOTHOUSE_SW_3_DOWN
-#define PIN_HOTHOUSE_SWITCH_7 PIN_HOTHOUSE_FSW_1
-#define PIN_HOTHOUSE_SWITCH_8 PIN_HOTHOUSE_FSW_2
+#define PIN_HOTHOUSE_SWITCH_1 PIN_HOTHOUSE_FSW_1
+#define PIN_HOTHOUSE_SWITCH_2 PIN_HOTHOUSE_FSW_2
+#define PIN_HOTHOUSE_SWITCH_3 PIN_HOTHOUSE_SW_1_UP
+#define PIN_HOTHOUSE_SWITCH_4 PIN_HOTHOUSE_SW_1_DOWN
+#define PIN_HOTHOUSE_SWITCH_5 PIN_HOTHOUSE_SW_2_UP
+#define PIN_HOTHOUSE_SWITCH_6 PIN_HOTHOUSE_SW_2_DOWN
+#define PIN_HOTHOUSE_SWITCH_7 PIN_HOTHOUSE_SW_3_UP
+#define PIN_HOTHOUSE_SWITCH_8 PIN_HOTHOUSE_SW_3_DOWN
 
 #define PIN_HOTHOUSE_POT_1 A1
 #define PIN_HOTHOUSE_POT_2 A2
